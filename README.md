1e3bb8142f0c30e50c2e38dc7d8e0fbd1d468dd1
Sam O'Connor <sam@octech.com.au>
2016-01-10 14:28:08 +1100

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,123 @@
-# InfoZip
+## Overview 
 
-[![Build Status](https://travis-ci.org/samoconnor/InfoZip.jl.svg?branch=master)](https://travis-ci.org/samoconnor/InfoZip.jl)
+This module provides support for reading and writing ZIP archives in Julia.
+
+[![Build Status](https://travis-ci.org/samoconnor/InfoZIP.jl.png)](https://travis-ci.org/samoconnor/InfoZIP.jl)
+
+## Installation
+
+Install via the Julia package manager, `Pkg.add("InfoZIP")`.
+
+Depends on the [Info ZIP](http://www.info-zip.org) `zip` and `uzip` tools.
+
+
+## High level interface
+
+Use `open_zip` open a ZIP Archive for read and/or write.
+
+Use `create_zip` to create a new ZIP Archive in one step.
+
+A ZIP Archive can be either a `.ZIP` file or an `Array{UInt8,1}`.
+
+
+## open_zip
+
+The result of `open_zip(archive)` is iterable and can be accessed as an
+Associative collection.
+
+```julia
+# Print size of each file in "foo.zip"...
+for (filename, data) in open_zip("foo.zip")
+    println("$filename has $(length(data)) bytes")
+end
+
+
+# Read contents of "bar.csv" from "foo.zip"...
+data = open_zip("foo.zip")["foo/bar.csv"]
+
+
+# Read "foo.zip" from in-memory ZIP archive...
+zip_data = http_get("http://foo.com/foo.zip")
+csv_data = open_zip(zip_data)["bar.csv"]
+
+
+# Create a Dict from a ZIP archive...
+Dict(open_zip("foo.zip"))
+Dict{AbstractString,Any} with 2 entries:
+  "hello.txt"    => "Hello!\n"
+  "foo/text.txt" => "text\n"
+
+
+# Create "foo.zip" with two files...
+open_zip("foo.zip", "w") do z
+    z["hello.txt"] = "Hello!\n"
+    z["bar.csv"] = "1,2,3\n"
+end
+
+
+# Create in-memory ZIP archive in "buf"...
+buf = UInt8[]
+open_zip(buf) do z
+    z["hello.txt"] = "Hello!\n"
+    z["bar.csv"] = "1,2,3\n"
+end
+http_put("http://foo.com/foo.zip", buf)
+
+
+# Add a new file to an existing archive"...
+open_zip("foo.zip", "r+") do z
+    z["newfile.csv"] = "1,2,3\n"
+end
+
+
+# Update an existing file in an archive"...
+open_zip("foo.zip", "r+") do z
+    z["newfile.csv"] = lowercase(z["newfile.csv"])
+end
+
+```
+
+
+## create_zip
+
+`create_zip([destination], content)` creates a ZIP archive from "content' in a single step. If "destination" is omitted the archive is returned as `Array{UInt8}`.
+
+```julia
+
+# Create archive from Dict...
+create_zip("foo.zip", Dict("hello.txt" => "Hello!\n",
+                           "bar.csv" => "1,2,3\n"))
+
+
+# Create archive from Pairs...
+create_zip("foo.zip", "hello.txt" => "Hello!\n",
+                      "bar.csv" => "1,2,3\n"))
+
+
+# Create archive from Tuples...
+zip_data = create_zip([("hello.txt", "Hello!\n"),
+                       ("bar.csv" => "1,2,3\n")])
+
+
+# Create archive from filenames array and data array...
+zip_data = create_zip(["hello.txt", "bar.csv"],
+                      ["Hello!\n",  "1,2,3\n"])
+
+# Create archive from names of files in the current directory...
+create_zip("foo.zip", ["hello.txt", "bar.csv"])
+or
+zip_data = create_zip(["hello.txt", "bar.csv"])
+```
+
+
+## unzip
+
+`unzip(archive, [outputdir])` extracts an archive to files in "outputdir" (or in the current directory by default).
+
+```julia
+unzip("foo.zip", "/tmp/")
+
+unzip(http_get("http://foo.com/foo.zip", "/tmp/"))
+```
+
+*Based on [fhs/ZipFile.jl#16](https://github.com/fhs/ZipFile.jl/pull/16), thanks @timholy*
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,350 @@
-using InfoZip
+using InfoZIP
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+
+# Command line "unzip" interface.
+
+function unzip_tool_is_missing()
+    try
+        readall(`unzip`)
+        return false
+    catch
+        println("WARNING: unzip tool not found!")
+        return true
+    end
+end
+
+# Unzip file to dict using external "unzip" tool.
+
+function test_unzip_file(z)
+    r = Dict()
+    for f in readlines(`unzip -Z1 $z`)
+        f = chomp(f)
+        if basename(f) != ""
+            r[f] = readall(`unzip -qc $z $f`)
+        end
+    end
+    return r
+end
+
+
+# Unzip zip data to dict using external "unzip" tool.
+
+function test_unzip(zip)
+    mktemp((tmp,io)-> begin
+        write(io, zip)
+        close(io)
+        return test_unzip_file(tmp)
+    end)
+end
+
+
+dict = Dict("hello.txt"     => "Hello!\n",
+            "foo/text.txt"  => "text\n")
+
+# In memory ZIP from Dict...
+@test dict == test_unzip(create_zip(dict))
+
+@test dict == Dict(open_zip(create_zip(dict)))
+
+@test open_zip(create_zip(dict))["hello.txt"] == "Hello!\n"
+
+@test open_zip(create_zip("empty" => ""))["empty"] == ""
+
+# In memory ZIP from pairs...
+@test dict == test_unzip(create_zip("hello.txt"     => "Hello!\n",
+                                   "foo/text.txt"  => "text\n"))
+
+# In memory ZIP from tuples...
+@test dict == test_unzip(create_zip(("hello.txt",     "Hello!\n"),
+                                    ("foo/text.txt",  "text\n")))
+
+# In memory ZIP from tuples...
+@test dict == test_unzip(create_zip([("hello.txt",     "Hello!\n"),
+                                    ("foo/text.txt",  "text\n")]))
+
+# In memory ZIP from arrays...
+@test dict == test_unzip(create_zip(["hello.txt", "foo/text.txt"],
+                                    ["Hello!\n", "text\n"]))
+
+# In memory ZIP using "do"...
+zip_data = UInt8[]
+open_zip(zip_data) do z
+    z["hello.txt"] = "Hello!\n"
+    z["foo/text.txt"] = "text\n"
+end
+@test dict == Dict(open_zip(zip_data))
+ 
+
+# ZIP to file from Dict...
+unzip_dict = ""
+z = tempname() * ".zip"
+#try
+    create_zip(z, dict)
+
+    @test unzip_tool_is_missing() || dict == test_unzip_file(z)
+
+    @test open_zip(z)["hello.txt"] == "Hello!\n"
+
+    @test dict == Dict(open_zip(z))
+
+    create_zip(z, "foo" => "bar")
+
+    @test open_zip(z)["foo"] == "bar"
+    @test !haskey(open_zip(z), "hello.txt")
+
+#finally
+#    rm(z)
+##end
+
+
+# ZIP to file from Pairs...
+unzip_dict = ""
+z = tempname() * ".zip"
+try
+    create_zip(z, "hello.txt"     => "Hello!\n",
+                  "foo/text.txt"  => "text\n")
+    @test unzip_tool_is_missing() || dict == test_unzip_file(z)
+    @test open_zip(z)["foo/text.txt"] == "text\n"
+finally
+    rm(z)
+end
+
+# Incremental ZIP to file...
+
+f = tempname() * ".zip"
+try
+    open_zip(f) do z
+        z["hello.txt"] = "Hello!\n"
+        z["foo/text.txt"] = "text\n"
+    end
+    @test dict == Dict(open_zip(f))
+    open_zip(f) do z
+        @test z["hello.txt"] == "Hello!\n"
+        @test z["foo/text.txt"] == "text\n"
+        @test z["foo/text.txt"] == "text\n" # read again
+    end
+
+    # Add one file...
+    open_zip(f) do z
+        z["newfile"] = "new!\n"
+    end
+    open_zip(f) do z
+        @test z["hello.txt"] == "Hello!\n"
+        @test z["newfile"] == "new!\n"
+        @test z["foo/text.txt"] == "text\n"
+    end
+
+    # Read and write (read first)...
+    open_zip(f) do z
+        z["hello.txt"] *= "World!\n"
+        z["foo/bar.txt"] = "bar\n"
+        z["foo/text.txt.copy"] = z["foo/text.txt"] * "Copy\n"
+    end
+    open_zip(f) do z
+        @test z["hello.txt"] == "Hello!\nWorld!\n"
+        @test z["foo/bar.txt"] == "bar\n"
+        @test z["foo/text.txt"] == "text\n"
+        @test z["foo/text.txt.copy"] == "text\nCopy\n"
+        @test z["foo/text.txt.copy"] == "text\nCopy\n" # read again
+        @test z["newfile"] == "new!\n"
+    end
+
+    # Read and write (write first)...
+    open_zip(f) do z
+        z["foo/bar.txt"] = "bar\n"
+        z["foo/text.txt.copy"] = z["foo/text.txt"] * "Copy\n"
+    end
+    open_zip(f) do z
+        @test z["hello.txt"] == "Hello!\nWorld!\n"
+        @test z["foo/bar.txt"] == "bar\n"
+        @test z["foo/text.txt"] == "text\n"
+        @test z["foo/text.txt.copy"] == "text\nCopy\n"
+        @test z["newfile"] == "new!\n"
+    end
+
+finally
+    rm(f)
+end
+
+# Write new file, then read...
+f = tempname() * ".zip"
+try
+    open_zip(f) do z
+        z["hello.txt"] = "Hello!\n"
+        z["foo/text.txt"] = "text\n"
+        @test z["hello.txt"] == "Hello!\n"
+    end
+    @test dict == Dict(open_zip(f))
+
+finally
+    rm(f)
+end
+
+
+# Write new file, then iterate...
+f = tempname() * ".zip"
+try
+    open_zip(f) do z
+        z["hello.txt"] = "Hello!\n"
+        z["foo/text.txt"] = "text\n"
+        @test [(n,v) for (n,v) in z] == [("hello.txt","Hello!\n"),("foo/text.txt","text\n")]
+    end
+    @test dict == Dict(open_zip(f))
+
+finally
+    rm(f)
+end
+
+
+# Write new file, then read and write...
+f = tempname() * ".zip"
+try
+    open_zip(f) do z
+        z["hello.txt"] = "Hello!\n"
+        z["foo/text.txt"] = "text\n"
+        @test z["foo/text.txt"] == "text\n"
+        @test z["hello.txt"] == "Hello!\n"
+        z["foo2/text.txt"] = "text\n"
+    end
+    open_zip(f) do z
+        @test z["hello.txt"] == "Hello!\n"
+        @test z["foo/text.txt"] == "text\n"
+        @test z["foo2/text.txt"] == "text\n"
+        @test_throws KeyError z["foo3/text.txt"]
+    end
+
+finally
+    rm(f)
+end
+
+
+
+# Incremental ZIP to buffer...
+
+buf = UInt8[]
+
+    open_zip(buf) do z
+        z["hello.txt"] = "Hello!\n"
+        z["foo/text.txt"] = "text\n"
+    end
+    @test dict == Dict(open_zip(buf))
+    open_zip(buf) do z
+        @test z["hello.txt"] == "Hello!\n"
+        @test z["foo/text.txt"] == "text\n"
+        @test z["foo/text.txt"] == "text\n" # read again
+    end
+
+    # Add one file...
+    open_zip(buf) do z
+        z["newfile"] = "new!\n"
+    end
+    open_zip(buf) do z
+        @test z["hello.txt"] == "Hello!\n"
+        @test z["newfile"] == "new!\n"
+        @test z["foo/text.txt"] == "text\n"
+    end
+
+    # Read and write (read first)...
+    open_zip(buf) do z
+        z["hello.txt"] *= "World!\n"
+        z["foo/bar.txt"] = "bar\n"
+        z["foo/text.txt.copy"] = z["foo/text.txt"] * "Copy\n"
+    end
+    open_zip(buf) do z
+        @test z["hello.txt"] == "Hello!\nWorld!\n"
+        @test z["foo/bar.txt"] == "bar\n"
+        @test z["foo/text.txt"] == "text\n"
+        @test z["foo/text.txt.copy"] == "text\nCopy\n"
+        @test z["foo/text.txt.copy"] == "text\nCopy\n" # read again
+        @test z["newfile"] == "new!\n"
+    end
+
+    # Read and write (write first)...
+    open_zip(buf) do z
+        z["foo/bar.txt"] = "bar\n"
+        z["foo/text.txt.copy"] = z["foo/text.txt"] * "Copy\n"
+    end
+    open_zip(buf) do z
+        @test z["hello.txt"] == "Hello!\nWorld!\n"
+        @test z["foo/bar.txt"] == "bar\n"
+        @test z["foo/text.txt"] == "text\n"
+        @test z["foo/text.txt.copy"] == "text\nCopy\n"
+        @test z["newfile"] == "new!\n"
+    end
+
+
+# Write new buffer, then read...
+buf = UInt8[]
+
+    open_zip(buf) do z
+        z["hello.txt"] = "Hello!\n"
+        z["foo/text.txt"] = "text\n"
+        @test z["hello.txt"] == "Hello!\n"
+    end
+    @test dict == Dict(open_zip(buf))
+
+
+
+# Write new buffer, then iterate...
+buf = UInt8[]
+
+    open_zip(buf) do z
+        z["hello.txt"] = "Hello!\n"
+        z["foo/text.txt"] = "text\n"
+        @test [(n,v) for (n,v) in z] == [("hello.txt","Hello!\n"),("foo/text.txt","text\n")]
+    end
+    @test dict == Dict(open_zip(buf))
+
+
+
+# Write new buffer, then read and write...
+buf = UInt8[]
+
+    open_zip(buf) do z
+        z["hello.txt"] = "Hello!\n"
+        z["foo/text.txt"] = "text\n"
+        @test z["foo/text.txt"] == "text\n"
+        @test z["hello.txt"] == "Hello!\n"
+        z["foo2/text.txt"] = "text\n"
+    end
+    open_zip(buf) do z
+        @test z["hello.txt"] == "Hello!\n"
+        @test z["foo/text.txt"] == "text\n"
+        @test z["foo2/text.txt"] == "text\n"
+    end
+
+
+
+# Unzip file created by command-line "zip" tool...
+
+testzip = joinpath(Pkg.dir("InfoZIP"),"test","test.zip")
+d = Dict(open_zip(testzip))
+@test sum(d["test.png"]) == 462242
+delete!(d, "test.png")
+@test dict == d
+
+
+# unzip()...
+
+mktempdir() do d
+    InfoZIP.unzip(testzip, d)
+    @test readall(joinpath(d, "hello.txt")) == "Hello!\n"
+    @test readall(joinpath(d, "foo/text.txt")) == "text\n"
+end
+
+mktempdir() do d
+    InfoZIP.unzip(create_zip(dict), d)
+    @test readall(joinpath(d, "hello.txt")) == "Hello!\n"
+    @test readall(joinpath(d, "foo/text.txt")) == "text\n"
+end
+
+
+mktempdir() do d
+    cd(d) do
+        open(io->write(io, "Hello!\n"), "hello.txt", "w")
+        mkdir("foo")
+        open(io->write(io, "text\n"), "foo/text.txt", "w")
+        @test dict == Dict(open_zip(create_zip(["hello.txt", "foo/text.txt"])))
+    end
+end
